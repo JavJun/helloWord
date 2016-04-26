3fe1f70aaf68e57608f60eabcc854eaba0a086c3
JAVIER <JAVIER@JAVIER-VAIO>
2016-04-26 13:18:38 -0500

    Subiendo proyecto

---
+++ helloWord/src/helloword/HelloWord.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+/**
+ *
+ * @author JAVIER
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
